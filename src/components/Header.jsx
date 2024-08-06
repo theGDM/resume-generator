bb373739adcca0f24bd79ac1445ec91f15e66731
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2024-08-07 00:11:40 +0530

    Gyandeep | Added Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,7 +40,7 @@ export default function Header() {
                             backgroundColor: colors.greenAccent[600], // Set your desired hover color
                         },
                         fontSize: '1.2rem',
-                        borderRadius: '0'
+                        borderRadius: '0.2rem'
                     }}
                     onClick={() => handleNavigation('login')}
                 >
@@ -59,7 +59,7 @@ export default function Header() {
                                 backgroundColor: colors.greenAccent[600], // Set your desired hover color
                             },
                             fontSize: '1.2rem',
-                            borderRadius: '0'
+                            borderRadius: '0.2rem'
                         }}
                         onClick={() => handleNavigation('register')}
                     >
@@ -78,7 +78,7 @@ export default function Header() {
                             backgroundColor: colors.redAccent[600], // Set your desired hover color
                         },
                         fontSize: '1.2rem',
-                        borderRadius: '0'
+                        borderRadius: '0.2rem'
                     }}
                     onClick={() => handleNavigation('logout')}
                 >
