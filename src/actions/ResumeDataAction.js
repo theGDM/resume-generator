288a3053f57277a7882f6d43c5c528080d6b9b5f
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2024-08-07 00:13:17 +0530

    Gyandeep | Created action for saving the Resume data

---
+++ src/actions/ResumeDataAction.js
@@ -0,0 +1,29 @@
+export const SetPersonalDetails = (data) => ({
+    type: 'SET_PERSONAL_DETAILS',
+    payload: data,
+});
+
+export const SetPortfolioLinks = (data) => ({
+    type: 'SET_PORTFOLIO_LINKS',
+    payload: data,
+});
+
+export const SetEducations = (data) => ({
+    type: 'SET_EDUCATIONS',
+    payload: data,
+});
+
+export const SetExperiences = (data) => ({
+    type: 'SET_EXPERIENCES',
+    payload: data,
+});
+
+export const SetCertifications = (data) => ({
+    type: 'SET_CERTIFICATIONS',
+    payload: data,
+});
+
+export const SetSkills = (data) => ({
+    type: 'SET_SKILLS',
+    payload: data,
+});
